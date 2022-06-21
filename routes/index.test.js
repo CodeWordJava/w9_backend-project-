2dac9f2c04dc18d217773734c44b4bb8f32471d0
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-06-21 11:35:11 +0100

    1st failing test written

---
+++ routes/index.test.js
@@ -0,0 +1,17 @@
+import app from "../app.js"
+import request from "supertest";
+import { test, expect } from "@jest/globals";
+
+
+
+
+
+describe("Testing GET route, checks status code is 200", () => {
+  
+  test("Should respond with status code is 200", async () => {
+    //Act:
+    const response = await request(app).get("/");
+    //Assert:
+    expect(response.statusCode).toBe(200)
+  });
+});
