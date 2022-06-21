6a4f005b364347533138bd2367747340455d9f10
unknown <kati_packer@hotmail.com>
2022-06-21 12:18:23 +0100

    first passing test

---
+++ app.js
@@ -0,0 +1,18 @@
+import express, { Router } from "express";
+import logger from 'morgan'
+import router  from "./routes/index.js";
+const PORT = 3000;
+const app = express();
+//import 'dovenv/config'
+
+//This console logs the server data
+app.use(logger('dev'))
+//This allow API to accept JSON data
+app.use(express.json())
+
+app.use("/",router)
+// app.listen(PORT, function(){
+// console.log (`Hello from the server on ${PORT}`)
+// });
+
+export default app
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+//import models( get by topic..)
+
+const router = Router()
+
+router.get ('/',async (req,res)=>{
+    res.json()
+})
+
+export default router
