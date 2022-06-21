d2bae4833e0f88a6a6a1bfe82e7152f8915c1708
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-06-21 14:37:51 +0100

    two main routes written

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,59 @@ import { Router } from "express";
 
 const router = Router()
 
-router.get ('/',async (req,res)=>{
+
+// basic GET route request from front-end
+router.get('/',async (req,res)=>{
     res.json()
+});
+
+
+// POST route to take in json object from front-end
+router.post('/', async (req, res) => {
+    
+    // const userName = req.body.userName;
+    // const link = req.body.link;
+    // const topic = req.body.topic;
+    const result = {link: "hobby", userName: "ali", topic: "react"};
+
+    // res.sendStatus(201);
+    res.json(result);
 })
 
+
+
+// catRouter.post("/", async function (req, res) {
+    
+//     // there is a bug here
+//     // if null or empty string is given it will still POST to the database
+//     // an if statement to catch bad/error data entries?
+    
+//     console.log(`This is the req.body.name ${name}`);
+//     console.log(`This is the req.body.human ${human}`);
+//     console.log(`This is the req.body.hobby ${hobby}`);
+    
+//     // console.log(`This is the result: ${result}`);
+//     res.json({success: true, payload: result});
+//   });
 export default router
+
+
+// json body should have:
+//      link
+//      uploader name
+//      topic (i.e. react)
+//      
+
+// router.post("/", async (req, res) => {
+//     const { username } = req.body;
+//     const createdUser = await usersModel.createUser({ username });
+  
+//     res.status(201).json({
+//       success: true,
+//       payload: createdUser,
+//     });
+//   });
+
+
+// PATCH 
+//      voteCount++
--- routes/index.test.js
+++ routes/index.test.js
@@ -1,8 +1,7 @@
 import app from "../app.js"
 import request from "supertest";
 import { test, expect } from "@jest/globals";
-
-
+// import { describe } from "yargs";
 
 
 
@@ -12,6 +11,39 @@ describe("Testing GET route, checks status code is 200.", () => {
     //Act:
     const response = await request(app).get("/");
     //Assert:
-    expect(response.statusCode).toBe(200)
+    expect(response.statusCode).toBe(200);
   });
+
+  // test(...BY TOPIC);
+
+});
+
+
+describe("POST should have three params: status code is 201", () => {
+
+  test("Should return status code is 201", async () => {
+    //Arrange.
+    const response = await request(app).post("/");
+    //Act.
+    //Assert.
+    // Changed to 200 to check OK status (201 = created)
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("Should have params: link", async () => {
+    // body should be an object.
+    // boby.link = expect.any(String)
+    // body.userName = expect.any(String)
+    // body.topic = expect.any(String)
+    //Arrange.
+    const expected = expect.objectContaining({link: expect.any(String), userName: expect.any(String), topic: expect.any(String)});
+    
+    //Act.
+    const response = await request(app).post("/");
+    
+    //Assert.
+    expect(response.body).toStrictEqual(expected)
+  });
+
+
 });
