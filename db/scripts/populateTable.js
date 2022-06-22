7c4451c9035f9bcf10e70fbdb0f72a7638e7363e
unknown <kati_packer@hotmail.com>
2022-06-22 16:42:06 +0100

    amended create table code

--- db/scripts/populateTable.js
+++ db/scripts/populateTable.js
@@ -5,7 +5,7 @@ import fakeData from "../lib.js";
 async function populateTable(){
  
     for (let i = 0; i < fakeData.length; i++) {
-      const res = await pool.query (`INSERT INTO tech_library (userName, link, topic)
+      const res = await pool.query (`INSERT INTO tech_links (link, userName, topic)
     VALUES ($1, $2, $3) RETURNING *;`,
     [fakeData[i].link, fakeData[i].userName, fakeData[i].topic] 
     );
