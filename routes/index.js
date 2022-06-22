acef40921c3a85f4cc5a7d7ee63066f35e7598d5
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-06-22 09:44:01 +0100

    updated plan

--- routes/index.js
+++ routes/index.js
@@ -4,58 +4,51 @@ import { Router } from "express";
 const router = Router()
 
 
+//GET by topic route
+//      returns a big object as an array of objects.
+//      will need a params of topic which we take from the req.params?
+//      
+//GET everything route
+//      returns everything from the database
+
 // basic GET route request from front-end
 router.get('/',async (req,res)=>{
-    res.json({link: "https://example.org/bottle.php#bone", userName: "Jase Pruitt", topic: "CSS"});
+    res.json({link: "https://example.org/bottle.php#bone", userName: "Jase Pruitt", topic: "CSS", votCount: 2});
 });
+
+
 
 
 // POST route to take in json object from front-end
 router.post('/', async (req, res) => {
+
+    // there is a bug here
+//     // if null or empty string is given it will still POST to the database
+//     // an if statement to catch bad/error data entries?
     
     // const userName = req.body.userName;
     // const link = req.body.link;
     // const topic = req.body.topic;
     const result = {link: "hobby", userName: "ali", topic: "react"};
 
-    // res.sendStatus(201);
-    res.json(result);
-})
-
-
-
-// catRouter.post("/", async function (req, res) {
-    
-//     // there is a bug here
-//     // if null or empty string is given it will still POST to the database
-//     // an if statement to catch bad/error data entries?
-    
-//     console.log(`This is the req.body.name ${name}`);
-//     console.log(`This is the req.body.human ${human}`);
-//     console.log(`This is the req.body.hobby ${hobby}`);
-    
-//     // console.log(`This is the result: ${result}`);
+     // console.log(`This is the result: ${result}`);
 //     res.json({success: true, payload: result});
-//   });
-export default router
 
-
-// json body should have:
-//      link
-//      uploader name
-//      topic (i.e. react)
-//      
-
-// router.post("/", async (req, res) => {
+//  // router.post("/", async (req, res) => {
 //     const { username } = req.body;
 //     const createdUser = await usersModel.createUser({ username });
   
 //     res.status(201).json({
 //       success: true,
 //       payload: createdUser,
-//     });
-//   });
+    // res.sendStatus(201);
+    res.json(result);
+});
 
-
+//PATCH ropute to update the voteCount on database
 // PATCH 
 //      voteCount++
+
+
+
+export default router
