b7bb62bce9a1d3ec5b26459de206eec0d0b62235
unknown <kati_packer@hotmail.com>
2022-06-22 10:50:46 +0100

    added cors

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import logger from 'morgan'
 import router  from "./routes/index.js";
 const PORT = process.env.port || 3000;
 const app = express();
+import cors from 'cors';
 //import 'dovenv/config'
+app.use(cors())
 
 //This console logs the server data
 app.use(logger('dev'))
