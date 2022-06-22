e7b04f145af2735c220ec48ab2af897a6752bed4
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-06-22 11:48:07 +0100

    updated case for table_name in models folder

--- models/index.js
+++ models/index.js
@@ -2,13 +2,13 @@ import pool from "../db/index.js";
 import "dotenv/config";
 
 export async function getEverything() {
-  const sqlString = `SELECT * FROM tech-links;`
+  const sqlString = `SELECT * FROM tech_links;`
   const response = await pool.query(sqlString);
   return response.rows
 }
 
 export async function createNewLink(userName, link, topic){
-  const sqlString = "INSERT INTO tech-links (userName, link, topic, voteCount) VALUES ($1, $2, $3, $4) RETURNING *;"
+  const sqlString = "INSERT INTO tech_links (userName, link, topic, voteCount) VALUES ($1, $2, $3, $4) RETURNING *;"
   const values = [userName, link, topic, 0]
   const data = await pool.query(sqlString, values);
   console.log(`This is the returning data ${data}`);
@@ -17,11 +17,11 @@ export async function createNewLink(userName, link, topic){
 
 
 
-export async function createNewCat(name, human, hobby) {
-  const sqlString = "INSERT INTO cats (name, human, hobby) VALUES ($1, $2, $3) RETURNING *;"
-  const values = [name, human, hobby]
-  // console.log(`This is the sql string ${sqlString}`);
-  const data = await query(sqlString, values);
-  console.log(`This is the returning data ${data}`);
-  return data;
-};
+// export async function createNewCat(name, human, hobby) {
+//   const sqlString = "INSERT INTO cats (name, human, hobby) VALUES ($1, $2, $3) RETURNING *;"
+//   const values = [name, human, hobby]
+//   // console.log(`This is the sql string ${sqlString}`);
+//   const data = await query(sqlString, values);
+//   console.log(`This is the returning data ${data}`);
+//   return data;
+// };
