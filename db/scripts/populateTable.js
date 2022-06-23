3394e6e2a1f34862c7501e44c78cf91b77b1935a
unknown <kati_packer@hotmail.com>
2022-06-23 11:47:36 +0100

    pop table

--- db/scripts/populateTable.js
+++ db/scripts/populateTable.js
@@ -5,10 +5,10 @@ import fakeData from "../lib.js";
 async function populateTable(){
  
     for (let i = 0; i < fakeData.length; i++) {
-      const res = await pool.query (`INSERT INTO tech_links (link, userName, topic)
+      const res = await pool.query (`INSERT INTO tech_links (link, userName, topic,votecount)
 
-    VALUES ($1, $2, $3) RETURNING *;`,
-    [fakeData[i].userName, fakeData[i].link, fakeData[i].topic] 
+    VALUES ($1, $2, $3, $4) RETURNING *;`,
+    [fakeData[i].link, fakeData[i].userName, fakeData[i].topic, 1] 
     );
     console.log (res.rows,"table populated")
  }
