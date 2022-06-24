9fbd5cc1064ad4b45a81ab560875129551c3fcc7
Luke <103851112+moeshy@users.noreply.github.com>
2022-06-24 12:15:53 +0100

    clean models

--- models/index.js
+++ models/index.js
@@ -2,31 +2,30 @@ import pool from "../db/index.js";
 import "dotenv/config";
 
 export async function getEverything() {
-  const sqlString = `SELECT * FROM tech_links;`
+  const sqlString = `SELECT * FROM tech_links;`;
   const response = await pool.query(sqlString);
-  return response.rows
-};
+  return response.rows;
+}
 
 export async function getByTopic(topic) {
-  const sqlString = `SELECT * FROM tech_links WHERE topic LIKE $1;` 
-  const values = [topic]
+  const sqlString = `SELECT * FROM tech_links WHERE topic LIKE $1;`;
+  const values = [topic];
   const data = await pool.query(sqlString, values);
   return data;
-};
+}
 
-export async function createNewLink(userName, link, topic){
-  const sqlString = "INSERT INTO tech_links (userName, link, topic, votecount) VALUES ($1, $2, $3, $4) RETURNING *;"
-  const values = [userName, link, topic, 1]
+export async function createNewLink(userName, link, topic) {
+  const sqlString =
+    "INSERT INTO tech_links (userName, link, topic, votecount) VALUES ($1, $2, $3, $4) RETURNING *;";
+  const values = [userName, link, topic, 1];
   const data = await pool.query(sqlString, values);
-  // console.log(`This is the returning data ${data}`);
   return data;
-};
+}
 
 export async function updateVoteCount(id) {
-  const sqlString = "UPDATE tech_links SET votecount = votecount + 1 WHERE id = $1;"
+  const sqlString =
+    "UPDATE tech_links SET votecount = votecount + 1 WHERE id = $1;";
   const values = [id];
-  console.log(`This from the models ${id}`)
   const data = await pool.query(sqlString, values);
-  console.log(`This the data returned from sql string ${data}`);
   return data;
 }
