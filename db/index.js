41cd1956ee34ae5e41527a506ad160a3c295903d
unknown <kati_packer@hotmail.com>
2022-07-17 12:10:38 +0100

    amended db credentials

--- db/index.js
+++ db/index.js
@@ -1,16 +1,24 @@
 import pg from "pg";
-import "dotenv/config"; //makes .env work
+import { db } from "../config/index.js";
+//import "dotenv/config"; //makes .env work
 
 // pools will use environment variables
 // for connection information
-const pool = new pg.Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  ssl: { rejectUnauthorized: false },
+// const pool = new pg.Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+//   ssl: { rejectUnauthorized: false },
+// });
+
+export const pool = new pg.Pool({
+  connectionString: db.DATABASE_URL,
+ 
+    ssl:{ rejectUnauthorized: false },
 });
+
 
 
 export default pool;
