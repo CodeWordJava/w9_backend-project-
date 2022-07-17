02a1652179099f39d69c408620052d674182f745
unknown <kati_packer@hotmail.com>
2022-07-17 14:36:26 +0100

    amended port as R10 heroku issue still happening

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express, { Router } from "express";
 import logger from "morgan";
 import router from "./routes/index.js";
 
-const PORT = process.env.port || 9000;
+const PORT = process.env.port || 3000;
 const app = express();
 
 //import 'dovenv/config'
